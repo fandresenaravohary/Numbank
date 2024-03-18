f8706db31173a4fa1067c5344c73eed0ca0443a4
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-18 23:57:41 +0300

    Feat: add the account list page

---
+++ src/app/accounts/page.js
@@ -0,0 +1,12 @@
+import AccountList from "@/components/AccountList";
+
+const AccountsPage = () => {
+    return (
+      <div>
+        <h1>Liste des comptes</h1>
+        <AccountList/>
+      </div>
+    );
+  };
+  
+  export default AccountsPage;
