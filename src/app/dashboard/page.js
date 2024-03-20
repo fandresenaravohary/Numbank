3d73fb904b09382b1f341b10622f38fd6e387a46
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-20 22:14:06 +0300

    style: turn the account table into a card and some css style

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,36 @@
+"use client"
+import AccountList from '@/components/AccountList'
 import TopNav from '@/components/TopNav'
 import React from 'react'
 
 export default function HomePage() {
+  const accounts = [
+    {
+      id: 1,
+      firstName: "Alice",
+      lastName: "Becker",
+      salary: 30000,
+      accountNumber: 1234567
+    },
+    {
+      id: 2,
+      firstName: "Kobe",
+      lastName: "Bryant",
+      salary: 400000,
+      accountNumber: 1987643
+    },
+    {
+      id: 3,
+      firstName: "Michael",
+      lastName: "Jordan",
+      salary: 1000000,
+      accountNumber: 1334567
+    }
+  ]
   return (
     <div>
         <TopNav/>
+        <AccountList accounts={accounts}/>
     </div>
   )
 }
--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,28 +1,16 @@
+import React from 'react';
+
 const AccountList = ({ accounts, handleModifyAccount }) => {
   return (
-    <div>
-      <table className="border-t border-b w-96 border-gray-200">
-        <thead>
-          <tr>
-            <th className="border-l border-r">First Name</th>
-            <th className="border-l border-r">Last Name</th>
-            <th className="border-l border-r">Salary</th>
-            <th className="border-l border-r">Account Number</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {accounts.map((account) => (
-            <tr key={account.id} className="border-t border-gray-200">
-              <td className="border-l border-r">{account.firstName}</td>
-              <td className="border-l border-r">{account.lastName}</td>
-              <td className="border-l border-r">{account.salary}</td>
-              <td className="border-l border-r">{account.accountNumber}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={() => handleModifyAccount(accounts.id)}>Edit</button>
+    <div className="grid grid-cols-1 gap-4 mt-3 ml-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {accounts.map((account) => (
+        <div key={account.id} className="bg-gray-200 rounded-lg shadow-md p-4">
+          <h2 className="text-lg font-semibold">{account.firstName} {account.lastName}</h2>
+          <p className="text-gray-600">Salary: {account.salary}</p>
+          <p className="text-gray-600">Account Number: {account.accountNumber}</p>
+          <button className="bg-blue-500 text-white rounded-md px-4 py-2 mt-2 hover:bg-blue-600" onClick={() => handleModifyAccount(account.id)}>Edit</button>
+        </div>
+      ))}
     </div>
   );
 };
