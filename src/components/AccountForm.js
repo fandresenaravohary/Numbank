f53241b93084d168ce1287aff1ca8ad47d07e29d
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-22 22:46:29 +0300

    refactor: refactored the code and add error message for empty fields

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,109 +1,94 @@
 'use client';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const AccountForm = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    birthDate: '',
-    salary: '',
-    accountNumber: ''
-  });
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const onSubmit = async (data) => {
+    // Envoi des données à l'API
+    console.log(data);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Ajoutez ici la logique pour envoyer les données au backend
-    console.log(formData);
-  };
+  return (
+    <form className="py-4 mt-4 mx-auto flex flex-row" onSubmit={handleSubmit(onSubmit)}>
+      <div className="border border-black p-4 max-w-xl w-96">
 
-
-    return (
-      <form className="py-4 mt-4 mx-auto flex flex-row" onSubmit={handleSubmit}>
-        <div className="border border-black p-4 max-w-xl w-96">
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="firstName" className="text-black">
-              FirstName
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              className="mt-1 w-full h-12 border border-black"
-              required
-            />
-          </div>
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="lastName" className="text-black">
-              Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              className="mt-1 w-full h-12 border border-black"
-              required
-            />
-          </div>
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="birthDate" className="text-black">
-              Birth Date
-            </label>
-            <input
-              type="date"
-              id="birthDate"
-              name="birthDate"
-              value={formData.birthDate}
-              onChange={handleChange}
-              className="mt-1 w-full h-12 border border-black"
-              required
-            />
-          </div>
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="salary" className="text-black">
-              Net monthly salary
-            </label>
-            <input
-              type="number"
-              id="salary"
-              name="salary"
-              value={formData.salary}
-              onChange={handleChange}
-              className="mt-1 w-full h-12 border border-black"
-              required
-            />
-          </div>
-          <div className="mb-4 flex flex-col">
-            <label htmlFor="accountNumber" className="text-black">
-              Account number
-            </label>
-            <input
-              type="text"
-              id="accountNumber"
-              name="accountNumber"
-              value={formData.accountNumber}
-              onChange={handleChange}
-              className="mt-1 w-full h-12 border border-black"
-              required
-            />
-          </div>
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-gray-400 p-3 text-white font-bold hover:bg-gray-600 w-48 mx-auto"
-            >
-              Add 
-            </button>
-          </div>
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="firstName" className="text-black">
+            FirstName
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            {...register('firstName', { required: true })}
+            className="mt-1 w-full h-12 border border-black"
+          />
+          {errors.firstName && <span className="text-red-500">Please, enter your firstName</span>}
         </div>
+
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="lastName" className="text-black">
+            Name
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            {...register('lastName', { required: true })}
+            className="mt-1 w-full h-12 border border-black"
+          />
+          {errors.lastName && <span className="text-red-500">Please, enter your name</span>}
+        </div>
+
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="birthDate" className="text-black">
+            Birth Date
+          </label>
+          <input
+            type="date"
+            id="birthDate"
+            name="birthDate"
+            {...register('birthDate', { required: true })}
+            className="mt-1 w-full h-12 border border-black"
+          />
+          {errors.birthDate && <span className="text-red-500">Please, enter your birth date</span>}
+        </div>
+
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="salary" className="text-black">
+            Salaire mensuel net
+          </label>
+          <input
+            type="number"
+            id="salary"
+            name="salary"
+            {...register('salary', { required: true })}
+            className="mt-1 w-full h-12 border border-black"
+          />
+          {errors.salary && <span className="text-red-500">Please, enter your salary</span>}
+        </div>
+
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="accountNumber" className="text-black">
+            Numéro de compte unique
+          </label>
+          <input
+            type="text"
+            id="accountNumber"
+            name="accountNumber"
+            {...register('accountNumber', { required: true })}
+            className="mt-1 w-full h-12 border border-black"
+          />
+          {errors.accountNumber && <span className="text-red-500">Please, enter your account number</span>}
+        </div>
+
+        <div className="flex justify-center">
+          <button type="submit" className="bg-gray-400 p-3 text-white font-bold hover:bg-gray-600 w-48 mx-auto">
+            Add an account
+          </button>
+        </div>
+      </div>
         <div className=" bg-gray-400 flex flex-col justify-center items-center text-white py-12 px-6 md:py-24 md:px-12 max-w-xl w-96">
           <h2 className="text-4xl font-bold mb-2">Hello, Friend</h2>
           <div className="border-2 w-10 border-white inline-block mb-2"></div>
