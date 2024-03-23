588c123546fe5f6d2322a9c1e0aab3e2818e27f2
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-23 21:59:27 +0300

    refactor: refactored the lists of accounts pages

--- src/app/accounts/page.js
+++ src/app/accounts/page.js
@@ -1,13 +1,19 @@
-"use client"
-import AccountList from '@/components/AccountList'
-import React from 'react'
-import { accountList } from '../data'
+import React from 'react';
+import AccountList from '@/components/AccountList';
+import { accountList } from '../data';
 
 export default function Accounts() {
+  // Fonction pour modifier un compte
+  const handleModifyAccount = (accountId) => {
+    // Implémentez ici la logique pour modifier le compte avec l'identifiant accountId
+    console.log(`Modify account with id: ${accountId}`);
+  };
+
   return (
     <div>
-      <h1>Lists of accounts</h1>
-      <AccountList accounts={accountList}/>
-  </div>
-  )
+      <h1>List of Accounts</h1>
+      <AccountList accounts={accountList} handleModifyAccount={handleModifyAccount} />
+    </div>
+  );
 }
+
