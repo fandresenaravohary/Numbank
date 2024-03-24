e3c994eed3943e9010f619f79966c1dd7cbb8cee
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-03-24 15:28:50 +0300

    Fix: button Edit on click

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,3 +1,4 @@
+import { accountList } from '@/app/data';
 import React from 'react';
 
 const AccountList = ({ accounts, handleModifyAccount }) => {
@@ -8,7 +9,7 @@ const AccountList = ({ accounts, handleModifyAccount }) => {
           <h2 className="text-lg font-semibold">{account.firstName} {account.lastName}</h2>
           <p className="text-gray-600">Salary: {account.salary}</p>
           <p className="text-gray-600">Account Number: {account.accountNumber}</p>
-          <button className="bg-blue-500 text-white rounded-md px-4 py-2 mt-2 hover:bg-blue-600" onClick={() => handleModifyAccount(account.id)}>Edit</button>
+          <button className="bg-blue-500 text-white rounded-md px-4 py-2 mt-2 hover:bg-blue-600" onClick={handleModifyAccount(account.id)}>Edit</button>
         </div>
       ))}
     </div>
