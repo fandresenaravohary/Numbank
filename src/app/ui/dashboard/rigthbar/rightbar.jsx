a2dd3b75199e692de908fb63b7739924a5e4be72
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-04-01 11:43:45 +0300

    Feat: add the rightbar component in the ui folder

---
+++ src/app/ui/dashboard/rigthbar/rightbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './rigthbar.module.css'
+
+const Rightbar = () => {
+  return (
+    <div className={styles.container}>Rightbar</div>
+  )
+}
+
+export default Rightbar;
