f90edd3a50ca06b2e530f2d401d4b9be6dfd85ef
Daris02 <hei.raymond.2@gmail.com>
2024-04-22 15:05:46 +0300

    fix: update

--- src/app/ui/dashboard/rigthbar/rightbar.jsx
+++ src/app/ui/dashboard/rigthbar/rightbar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
 import styles from './rigthbar.module.css'
+import { getAllAccounts, getLoanInfo } from "../../../../utils/api/data";
 
 const Rightbar = () => {
+  const [accounts, setAccounts] = useState([]);
+  const [loanInfos, setLoanInfos] = useState([]);
+
+  useEffect(() => {
+    getAllAccounts().then((data) => {
+      setAccounts(data);
+    })
+    getAllInfoLoan();
+  }, [])
+
+  const getAllInfoLoan = () => {
+    let loanLists = [];
+    console.log("-----Function Loop-------");
+    accounts.forEach(account => {
+      getLoanInfo(account.id).then((data) => {
+        const loanInfo = {
+          loan: data.loan,
+          balance: data.balanace,
+          loanInterest: data.loanInterest,
+          name: account.customerFirstName,
+          id: account.id,
+        }
+        console.log(loanInfo)
+        loanLists.push(loanInfo);
+      })
+    });
+
+    // console.log(loanLists);
+    // setLoanInfos(loanLists);
+  }
+
+  // console.log(loanInfos);
+  
   return (
-    <div className={styles.container}>Rightbar</div>
+    <div className={styles.container}>
+      <h1>Account Info</h1>
+      {
+        loanInfos.map((account) => (
+          <div key={account.id}>
+            <h2>{account.customerFirstName}</h2>
+            <div>
+
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- src/utils/api/data.js
+++ src/utils/api/data.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080";
+
 const getAllAccounts = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/accounts");
+    const response = await axios.get(`${BASE_URL}/accounts`);
     return response.data;
   } catch (error) {
     console.error("An error backend:", error);
@@ -13,16 +15,23 @@ const sendFormDataToBackend = async (formData) => {
   const dataToSend = [];
   dataToSend.push(formData);
   try {
-    const response = await axios.post(
-      "http://localhost:8080/accounts",
-      dataToSend
-    );
+    const response = await axios.post(`${BASE_URL}/accounts`, dataToSend);
   } catch (error) {
     console.log(error);
   }
 };
 
+const getLoanInfo = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/balance/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export { 
   getAllAccounts,
-  sendFormDataToBackend,  
+  sendFormDataToBackend,
+  getLoanInfo,
 };
